9e5aba1fb7deab71353dda3124c37b7c38f8002f
Miguel Ángel Ciórraga <maciorraga@gmail.com>
2015-05-21 19:23:39 +0200

    Cambios varios
    - Añadido favicon
    - Modificada imagen de usuario predeterminada
    - Creados los mensaje de error en la sección "Mi cuenta"
    - Añadida nueva imagen como fondo de web

--- public/index.php
+++ public/index.php
@@ -115,24 +115,36 @@ $app->post('/registro', function() use ($app) {
     }
 });
 
+//Cuando pulsamos el botón actualizar en la sección "Mi cuenta"
 $app->post('/actualizaUsuario', function() use ($app) {
-    //var_dump($_POST);die();
-    $userAModificar = ORM::for_table('usuario')->find_one($_SESSION['usuarioLogin']['id']);
-    $userAModificar->user = $_POST['user'];
-    $userAModificar->password = $_POST['pass1'];
-    $userAModificar->nombre = $_POST['nombre'];
-    $userAModificar->apellidos = $_POST['apellidos'];
-    $userAModificar->email = $_POST['email'];
-    $userAModificar->steam = $_POST['steam'];
-    $userAModificar->edad = $_POST['edad'];
-    $userAModificar->save();
+    if(!$_POST['user'] || !$_POST['pass1'] || !$_POST['pass2'] || !$_POST['email'] || !$_POST['steam'] || !$_POST['nombre'] || !$_POST['edad']){
+        $app->render('miCuenta.html.twig', array('msgCuenta' => array("danger","Debes rellenar todos los campos obligatorios")));
+        die();
+    }else{
+        if ($_POST['pass1'] == $_POST['pass2']) {
+            $userAModificar = ORM::for_table('usuario')->find_one($_SESSION['usuarioLogin']['id']);
+            $userAModificar->user = $_POST['user'];
+            $userAModificar->password = $_POST['pass1'];
+            $userAModificar->nombre = $_POST['nombre'];
+            $userAModificar->apellidos = $_POST['apellidos'];
+            $userAModificar->email = $_POST['email'];
+            $userAModificar->steam = $_POST['steam'];
+            $userAModificar->edad = $_POST['edad'];
+            $userAModificar->save();
 
-    $usuario = ORM::for_table('usuario')->where('id', $_SESSION['usuarioLogin']['id'])->find_one();
-    if($usuario){
-        $_SESSION['usuarioLogin'] = $usuario;
+            $usuario = ORM::for_table('usuario')->where('id', $_SESSION['usuarioLogin']['id'])->find_one();
+            if($usuario){
+                $_SESSION['usuarioLogin'] = $usuario;
+            }
+
+            $app->render('miCuenta.html.twig', array('msgCuenta' => array("success","Cambios realizados con éxito")));
+            die();
+        }else{
+            $app->render('miCuenta.html.twig', array('msgCuenta' => array("danger","Las contraseñas no son iguales")));
+            die();
+        }
     }
 
-    $app->render('miCuenta.html.twig', array('update' => "1"));
 
 });
 
