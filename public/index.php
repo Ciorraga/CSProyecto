86c1637c9768af8951de76bf86eac69e6f381a3e
Miguel Ángel Ciórraga <maciorraga@gmail.com>
2015-06-15 18:19:22 +0200

    Equipos
    - Añadida la tabla Mis retos a la sección equipos

--- public/index.php
+++ public/index.php
@@ -160,9 +160,9 @@ $app->get('/equipos', function() use ($app) {
             ->where('equipo_id',$equipo[0]['id'])
             ->find_many();
 
-        $_SESSION['numMensajes'] = ORM::for_table('mensaje')
-            ->where('usuario_id', $_SESSION['usuarioLogin']['id'])
-            ->where('leido',0)->count();
+        $ultJugados = ORM::for_table('reto')
+            ->raw_query('select reto.ganador,eq1.nombre as nombreEq1,eq2.nombre as nombreEq2,eq1.logo as eq1Imagen,eq2.logo as eq2Imagen,reto.fecha,reto.mapa,reto.res_eq_retador as resEq1,reto.res_eq_retado as resEq2 from reto join equipo as eq1 on reto.retador_id=eq1.id join equipo as eq2 on reto.retado_id=eq2.id where (reto.retador_id='.$_SESSION['usuarioLogin']['equipo_id'].' or reto.retado_id='.$_SESSION['usuarioLogin']['equipo_id'].') and reto.ganador IS NOT NULL  ORDER BY reto.fecha DESC')
+            ->find_many();
 
         if($_SESSION['usuarioLogin']['id']==$equipo[0]['id']){
             $miEquipo = false;
@@ -174,7 +174,8 @@ $app->get('/equipos', function() use ($app) {
         $req->mostrarSolicitudes($_SESSION['usuarioLogin']['id']);
         $req->mostrarMensajes($_SESSION['usuarioLogin']['id']);
 
-        $app->render('equipos.html.twig',array('imagenUser'=>$_SESSION['usuarioLogin']['imagen'],'usuarioLogin'=>$_SESSION['usuarioLogin'],'numMensajes' => $_SESSION['numMensajes'],'equipo' => $equipo,'usuarios' => $usuarios,'miEquipo' => $miEquipo,'nuevaSolicitud' => $_SESSION['solicitudes']));
+        $app->render('equipos.html.twig',array('imagenUser'=>$_SESSION['usuarioLogin']['imagen'],'usuarioLogin'=>$_SESSION['usuarioLogin'],'numMensajes' => $_SESSION['numMensajes'],'equipo' => $equipo,'usuarios' => $usuarios,'miEquipo' => $miEquipo,'nuevaSolicitud' => $_SESSION['solicitudes'],'misRetos' => $ultJugados));
+        die();
     }
 
 
@@ -237,12 +238,16 @@ $app->get('/equipos/:equipo', function ($equipo) use ($app) {
         $botonSolicitud = false;
     }
 
+    $ultJugados = ORM::for_table('reto')
+        ->raw_query('select reto.ganador,eq1.nombre as nombreEq1,eq2.nombre as nombreEq2,eq1.logo as eq1Imagen,eq2.logo as eq2Imagen,reto.fecha,reto.mapa,reto.res_eq_retador as resEq1,reto.res_eq_retado as resEq2 from reto join equipo as eq1 on reto.retador_id=eq1.id join equipo as eq2 on reto.retado_id=eq2.id where (reto.retador_id='.$equipo[0]['id'].' or reto.retado_id='.$equipo[0]['id'].') and reto.ganador IS NOT NULL  ORDER BY reto.fecha DESC')
+        ->find_many();
+
     $req = new comun();
     $req->mostrarSolicitudes($_SESSION['usuarioLogin']['id']);
     $req->mostrarMensajes($_SESSION['usuarioLogin']['id']);
     $imagenUser = $_SESSION['usuarioLogin']['imagen'];
 
-    $app->render('equipos.html.twig',array('imagenUser'=>$imagenUser,'usuarioLogin'=>$_SESSION['usuarioLogin'],'numMensajes' => $_SESSION['numMensajes'],'equipo' => $equipo,'usuarios' => $usuarios,'botonSolicitud' => $botonSolicitud,'nuevaSolicitud' => $_SESSION['solicitudes']));
+    $app->render('equipos.html.twig',array('imagenUser'=>$imagenUser,'usuarioLogin'=>$_SESSION['usuarioLogin'],'numMensajes' => $_SESSION['numMensajes'],'equipo' => $equipo,'usuarios' => $usuarios,'botonSolicitud' => $botonSolicitud,'nuevaSolicitud' => $_SESSION['solicitudes'],'misRetos' => $ultJugados));
 });
 
 $app->get('/solicitudes', function() use ($app) {
