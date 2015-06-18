0cc54aa629cc4037f6b71993f4da65f79c127bfd
Miguel Ángel Ciórraga <maciorraga@gmail.com>
2015-06-18 21:56:48 +0200

    Administración
    - Borrado de retos

--- public/controller/administracion/botonesAdministracion.php
+++ public/controller/administracion/botonesAdministracion.php
@@ -94,9 +94,22 @@ if(isset($_POST['fijaResultadoReto'])){
     $retosEq = ORM::for_table('reto')
         ->raw_query('select eq1.nombre as nombreEq1,eq2.nombre as nombreEq2,eq1.logo as eq1Imagen,eq2.logo as eq2Imagen,reto.id,reto.fecha,reto.mapa,reto.res_eq_retador as resEq1,reto.res_eq_retado as resEq2 from reto join equipo as eq1 on reto.retador_id=eq1.id join equipo as eq2 on reto.retado_id=eq2.id where reto.ganador IS null AND reto.aceptado IS NOT false ORDER BY reto.fecha ASC limit 10')
         ->find_many();
-    $retos1vs1 = 23;
 
     $app->render('admin/listaRetos.html.twig',array('retosEq' => $retosEq, 'mensajeOk' => 'Reto actualizado'));
+    die();
+}
+
+if(isset($_POST['botonBorrarReto'])){
+    $reto = ORM::for_table('reto')
+        ->where('id',$_POST['botonBorrarReto'])
+        ->find_one();
+    $reto->delete();
+
+    $retosEq = ORM::for_table('reto')
+        ->raw_query('select eq1.nombre as nombreEq1,eq2.nombre as nombreEq2,eq1.logo as eq1Imagen,eq2.logo as eq2Imagen,reto.id,reto.fecha,reto.mapa,reto.res_eq_retador as resEq1,reto.res_eq_retado as resEq2 from reto join equipo as eq1 on reto.retador_id=eq1.id join equipo as eq2 on reto.retado_id=eq2.id where reto.ganador IS null AND reto.aceptado IS NOT false ORDER BY reto.fecha ASC limit 10')
+        ->find_many();
+
+    $app->render('admin/listaRetos.html.twig',array('retosEq' => $retosEq, 'mensajeOk' => 'Reto borrado'));
     die();
 }
 
