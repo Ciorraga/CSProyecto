2df5ddbfbbefdd72a2d8f9f6fade6a3730805100
Miguel Ángel Ciórraga <maciorraga@gmail.com>
2015-06-19 02:25:23 +0200

    Reportes
    - Habilitado el reporte de los comentarios de las noticias

--- public/controller/inicio/inicio.php
+++ public/controller/inicio/inicio.php
@@ -15,6 +15,28 @@ $app->get('/', function() use ($app) {
         $notic = $req->mostrarNoticias();
         $req->mostrarSolicitudes($_SESSION['usuarioLogin']['id']);
         $req->mostrarMensajes($_SESSION['usuarioLogin']['id']);
-        $app->render('inicio.html.twig', array('imagenUser'=>$_SESSION['usuarioLogin']['imagen'],'nuevaSolicitud' => $_SESSION['solicitudes'],'noticias' => $notic,'numMensajes' => $_SESSION['numMensajes'],'usuarioLogin' => $_SESSION['usuarioLogin'],'registrado' => 'env'));
+        $app->render('inicio.html.twig', array('imagenUser'=>$_SESSION['usuarioLogin']['imagen'],
+            'nuevaSolicitud' => $_SESSION['solicitudes'],
+            'noticias' => $notic,
+            'numMensajes' => $_SESSION['numMensajes'],
+            'usuarioLogin' => $_SESSION['usuarioLogin'],
+            'registrado' => 'env'));
     }
 })->name('inicio');
+
+$app->get('/reportarComentario/:id', function ($id) {
+    $compReportado = ORM::for_table('reporte_comentario')
+        ->where('comentario_id',$id)
+        ->find_one();
+
+    if(!$compReportado){
+        $fechaYHora = date("Y-m-d H:i:s");
+        $reporte = ORM::for_table('reporte_comentario')
+            ->create();
+        $reporte->usuario_id = $_SESSION['usuarioLogin']['id'];
+        $reporte->comentario_id = $id;
+        $reporte->fecha = $fechaYHora;
+        $reporte->save();
+    }
+    echo "Ok";
+})->name('reportaComentario');
