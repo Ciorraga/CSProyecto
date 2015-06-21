3d7313aac9978afca1ea24eac2eaa00404660918
Miguel Ángel Ciórraga <maciorraga@gmail.com>
2015-06-21 03:31:54 +0200

    Administración
    - Habilitado el borrado de retos 1vs1

--- public/controller/administracion/botonesAdministracion.php
+++ public/controller/administracion/botonesAdministracion.php
@@ -166,6 +166,32 @@ if(isset($_POST['botonBorrarReto'])){
     die();
 }
 
+if(isset($_POST['botonBorrarReto1vs1'])){
+    $reto = ORM::for_table('reto1vs1')
+        ->where('id',$_POST['botonBorrarReto1vs1'])
+        ->find_one();
+    $reto->delete();
+
+    $retosAbiertos = ORM::for_table('reto1vs1')
+        ->raw_query("select us1.user as user1,us2.user as user2,us1.imagen as us1Imagen,us2.imagen as us2Imagen,reto1vs1.fecha,reto1vs1.mapa,reto1vs1.id
+        from reto1vs1 join usuario as us1 on reto1vs1.retador_id=us1.id join usuario as us2 on reto1vs1.retado_id=us2.id where reto1vs1.ganador IS null  ORDER BY reto1vs1.fecha ASC")
+        ->find_many();
+
+    $retosCerrados = ORM::for_table('reto')
+        ->raw_query("select us1.user as user1,us2.user as user2,us1.imagen as us1Imagen,us2.imagen as us2Imagen,reto1vs1.fecha,reto1vs1.mapa,reto1vs1.res_retador as resUs1,reto1vs1.res_retado as resUs2,reto1vs1.id
+        from reto1vs1 join usuario as us1 on reto1vs1.retador_id=us1.id join usuario as us2 on reto1vs1.retado_id=us2.id where reto1vs1.ganador IS NOT null ORDER BY reto1vs1.fecha ASC")
+        ->find_many();
+
+    $req = new comun();
+    $ret = $req->compruebaRetos();
+
+    $app->render('admin/listaRetos1vs1.html.twig',array('retosEq' => $retosAbiertos,
+        'retNum' => $ret,
+        'mensajeOk' => 'Reto borrado',
+        'retosCerrados' => $retosCerrados));
+    die();
+}
+
 if(isset($_POST['botonBorrarReporte'])){
     $reporte = ORM::for_table('reporte_comentario')
         ->where('id',$_POST['botonBorrarReporte'])
